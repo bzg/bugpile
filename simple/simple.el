89ed6662b1597e620b44f9440c5c9ec6e03bf5a8
tj <tj@data-driven.de>
2012-06-05 01:01:01 +0200

    very preliminary attempt to insert html form at the right place in after todo heading in html export

--- simple/simple.el
+++ simple/simple.el
@@ -1,18 +1,50 @@
-m;;; simple.el -- iorg proof of concept
+;;; simple.el -- iorg proof of concept
 (require 'org)
 (require 'elnode)
+(require 'org-export)
 
-(add-hook 'org-export-html-final-hook 'iorg-html-postprocess)
 
-(defvar iorg-html-postprocess-re "...TODO..."
-  "Match headlines in exported html.")
+;;;; Declare functions
+(declare-function org-entry-is-todo-p "org" nil)
+(declare-function org-get-todo-state "org" nil)
+
+;; ;; obsolete - old exporter
+;; (add-hook 'org-export-html-final-hook 'iorg-html-postprocess)
+
+
+;; (defvar org-export-filter-final-output-functions nil
+;;   "List of functions applied to the transcoded string.
+;; Each filter is called with three arguments: the full transcoded
+;; string, the back-end, as a symbol, and the communication channel,
+;; as a plist.  It must return a string that will be used as the
+;; final export output.")
+
+(add-to-list 'org-export-filter-final-output-functions
+             'iorg-html-postprocess)
+
+
+(defvar iorg-html-postprocess-begin-txt-regexp
+  "<div class=\"outline-text.*\">"
+  "Match the beginning of outline text in exported html.")
+
+(defvar iorg-html-postprocess-todo-regexp
+  "\\(<span class=\"todo \\)\\([A-Z]+\\)\\(\">\\)"
+  "Match todo items in exported html.")
+
 
 (defun iorg-html-postprocess ()
   "Add buttons to HTML export to make headlines editable."
   ;; TODO: (2) adding buttons to html export
   (save-excursion
     (goto-char (point-min))
-    (while (re-search-forward iorg-html-postprocess-re))))
+    (while (re-search-forward iorg-html-postprocess-todo-regexp)
+      (re-search-forward iorg-html-postprocess-begin-txt-regexp)
+      (re-search-backward "<div")
+      (insert
+       "<form action=\"URI\" method=\"Methode\" enctype=\"MIME-Typ\">
+<!-- Formularelemente und andere Elemente innerhalb des
+Formulars -->
+</form>"))))
 
 (defun iorg-launch (port)
   "Launch the elnode server which will serve and edit simple.org."

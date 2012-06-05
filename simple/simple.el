87d5e1392d88b4cd9fa7e1726734c24afb2e5ac8
tj <tj@data-driven.de>
2012-06-05 10:43:29 +0200

    second draft html-process function in simple.el

--- simple/simple.el
+++ simple/simple.el
@@ -32,10 +32,14 @@
   "Match todo items in exported html.")
 
 
-(defun iorg-html-postprocess ()
+(defun iorg-html-postprocess (str back chan)
   "Add buttons to HTML export to make headlines editable."
   ;; TODO: (2) adding buttons to html export
-  (save-excursion
+  (let ((transc-str str)
+        (back-end back)
+        (comm-chan chan))
+  (with-temp-buffer 
+    (insert transc-str)
     (goto-char (point-min))
     (while (re-search-forward iorg-html-postprocess-todo-regexp)
       (re-search-forward iorg-html-postprocess-begin-txt-regexp)
@@ -44,7 +48,8 @@
        "<form action=\"URI\" method=\"Methode\" enctype=\"MIME-Typ\">
 <!-- Formularelemente und andere Elemente innerhalb des
 Formulars -->
-</form>"))))
+</form>"))
+    (buffer-string))))
 
 (defun iorg-launch (port)
   "Launch the elnode server which will serve and edit simple.org."

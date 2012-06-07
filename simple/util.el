4eeb69e648ade20ab883bcdc47562fc534e94f60
tj <tj@data-driven.de>
2012-06-07 17:27:56 +0200

    Use case 'changing todo to done' finished (untested). Made assoc-re in util.el even more powerfull.

--- simple/util.el
+++ simple/util.el
@@ -6,9 +6,20 @@
     (when (funcall proc (car pair))
       (return pair))))
 
-(defun assoc-re (re list)
-  "Return the first pair in LIST whose `car' matches RE."
-  (assoc-proc (lambda (it) (string-match re (format "%s" it))) list))
+(defun assoc-re (re list &optional count)
+  "Return the first pair in LIST whose `car' matches RE. If COUNT is non nil, return instead the text matched by `string-match'. If COUNT is zero, return the entire matched text, otherwise just the portion corresponding to the COUNTth parenthetical subexpression."
+  (let* ((in-string)
+         (assoc
+          (assoc-proc
+           (lambda (it)
+             (let ((str (format "%s" it)))
+               (and
+                (string-match re str)
+                (setq in-string str))))
+               list)))
+         (if (and count (integerp count))
+             (match-string count in-string)
+           assoc)))
 
 (defmacro while-visiting-file (file &rest body)
   "Execute BODY in a temporary buffer visiting FILE."

483392d146444abbf771a43cc92a0427b9b44c8b
tj <tj@data-driven.de>
2012-06-11 05:59:11 +0200

    Some regexp and functions written in simple.el.

--- simple/simple.el
+++ simple/simple.el
@@ -5,39 +5,108 @@
 (require 'org-e-html)
 (require 'org-agenda)
 (require 'util)
-(require 'iorg-element)
-(require 'iorg-e-html)
 
 ;;;; Declare functions
 (declare-function org-entry-is-todo-p "org" nil)
 (declare-function org-get-todo-state "org" nil)
 (declare-function org-check-for-org-mode "org-agenda" nil)
 
-(add-to-list 'org-export-filter-final-output-functions
-             'iorg-html-postprocess)
+;; (add-to-list 'org-export-filter-final-output-functions
+;;              'iorg-html-postprocess)
 
-(defvar iorg-html-begin-txt-regexp
+(add-to-list 'org-export-filter-headline-functions
+             'iorg-html-wrap-headline-content-in-textarea)
+
+(add-to-list 'org-export-filter-headline-functions
+             'iorg-html-wrap-headline-in-form)
+
+(add-to-list 'org-export-filter-headline-functions
+             'iorg-html-wrap-headline-todo-in-select)
+
+(add-to-list 'org-export-filter-headline-functions
+             'iorg-html-wrap-headline-string-in-text)
+
+(add-to-list 'org-export-filter-headline-functions
+             'iorg-html-wrap-headline-tags-in-text)
+
+ 
+(defvar iorg-html-outline-text-regexp
   (concat
+   ;; div
+   "\\(<div\\)"
+   ;; blank(s)
+   "\\([[:blank:]]+\\)"
    ;; class
-   "\\(<div class=\"outline-text-\\)\\([-[:digit:]]+\\)\\(\"\\)"
+   "\\(class=\"outline-text-\\)\\([-[:digit:]]+\\)\\(\"\\)"
    ;; blank(s)
    "\\([[:blank:]]+\\)"
    ;; id
    "\\(id=\"text\\)\\([-[:digit:]]+\\)\\(\">\\)")
   "Match the beginning of outline text in exported html.")
 
+(defvar iorg-html-outline-container-regexp
+  (concat
+   ;; div
+   "\\(<div\\)"
+   ;; blank(s)
+   "\\([[:blank:]]+\\)"
+   ;; id
+   "\\(id=\"outline-container\\)\\([-[:digit:]]+\\)\\(\"\\)"
+   ;; blank(s)
+   "\\([[:blank:]]+\\)"
+   ;; class
+   "\\(class=\"outline\\)\\([-[:digit:]]+\\)\\(\">\\)")
+  "Match the beginning of an outline-container in exported html.")
+
 (defvar iorg-html-todo-regexp
-  "\\(<span class=\"todo \\)\\([A-Z]+\\)\\(\">\\)"
+  (concat
+   ;; span
+  "\\(<span\\)"
+  ;; blank(s)
+   "\\([[:blank:]]+\\)"
+  ;; class
+  "\\(class=\"todo\\|class=\"done\\)"
+  ;; blank(s)
+   "\\([[:blank:]]+\\)"
+  ;; TODO
+  "\\([A-Z]+\\)\\(\">\\)")
   "Match todo items in exported html.")
+
+(defvar iorg-html-tag-regexp
+  (concat
+   ;; span
+  "\\(<span\\)"
+  ;; blank(s)
+   "\\([[:blank:]]+\\)"
+  ;; class
+  "\\(class=\"tag\">\\)")
+  "Match todo items in exported html.")
+
+(defvar iorg-html-section-number-regexp
+  (concat
+   ;; span
+  "\\(<span\\)"
+  ;; blank(s)
+   "\\([[:blank:]]+\\)"
+  ;; class
+  "\\(class=\"section-number\\)\\([-[:digit:]]+\\)\\(\">\\)")
+  "Match section-number in exported html.")
 
 (defvar iorg-html-div-end-repexp
   "</div>"
   "Match end of <div> sections")
 
+(defvar iorg-html-span-end-repexp
+  "</span>"
+  "Match end of <span> sections")
+
+;; (defvar iorg-input-submit-regexp
+;;   "<input type=\"submit\""
+;;   "Match submit button in exported html.")
+
 (defvar iorg-alist-outline-regexp
   "\\(outline\\)\\([-[:digit:]]+\\)"
   "Match key in http-params alist that identifies the outline level.")
-
 
 (defconst simple-dir
   (file-name-directory (or load-file-name (buffer-file-name)))
@@ -45,7 +114,8 @@
 
 (defconst simple-urls
   '(("^$"      . iorg-initialize-simple-handler)
-    ("^todo/$" . iorg-change-state-handler)))
+    ("^todo/$" . iorg-change-state-handler)
+    ("^edit/$" . iorg-edit-headline-handler)))
 
 (defun simple-dispatcher-handler (httpcon)
   "Dispatch requests to the 'simple' app"
@@ -60,7 +130,7 @@
     (goto-char (point-min))
     (while (and
             (re-search-forward iorg-html-todo-regexp nil t)
-            (re-search-forward iorg-html-begin-txt-regexp nil t))
+            (re-search-forward iorg-html-outline-text-regexp nil t))
       (goto-char (match-beginning 0))
       (insert
        (concat
@@ -68,6 +138,98 @@
         "  <input type=\"submit\" value=\" Finish \" name=\"outline-1\">"
         "</form>")))
     (buffer-substring-no-properties (point-min) (point-max))))
+
+
+(defun iorg-html-wrap-headline-todo-in-select (transc-str back-end comm-chan) 
+  "Wrap the todo part of the (BACK-END = html) transcoded Org headline TRANSC-STR into a html select-field, using information from the communication channel COMM-CHAN."
+    (with-temp-buffer
+    (insert transc-str)
+    (goto-char (point-min))
+    (while (re-search-forward iorg-html-todo-regexp nil t)
+      (goto-char (match-end 0))
+      (insert
+       (concat
+        "<select "
+        "name=\"simple-todo\""
+        "size=\"1\">"
+        "<option selected>TODO</option>"
+        "<option>DONE</option>")))
+    (while (re-search-forward iorg-html-span-end-repexp nil t)
+      (goto-char (match-beginning 0))
+      (insert "<\select>"))
+    (buffer-substring-no-properties (point-min) (point-max))))
+
+(defun iorg-html-wrap-headline-tags-in-text (transc-str back-end comm-chan) 
+  "Wrap the tags in the (BACK-END = html) transcoded Org headline TRANSC-STR into one html text-field each, using information from the communication channel COMM-CHAN."
+    (with-temp-buffer
+    (insert transc-str)
+    (goto-char (point-min))
+    (while (re-search-forward iorg-html-tag-regexp nil t)
+      (goto-char (match-end 0))
+      (insert
+       (concat
+        "<input "
+        "type=\"text\""
+        "name=\"simple-tag\""
+        "size=\"40\""
+        "maxlenght=\"80\">")))
+    (while (re-search-forward iorg-html-span-end-repexp nil t)
+      (goto-char (match-end 0))
+      (insert "<\input>"))
+    (buffer-substring-no-properties (point-min) (point-max))))
+
+(defun iorg-html-wrap-headline-string-in-text (transc-str back-end comm-chan) 
+  "Wrap the headline string in the (BACK-END = html) transcoded Org headline TRANSC-STR into a html text-field, using information from the communication channel COMM-CHAN."
+    (with-temp-buffer
+    (insert transc-str)
+    (goto-char (point-min))
+    (while (and
+            (re-search-forward "&nbsp;&nbsp;&nbsp;" nil t)
+            (re-search-backward iorg-html-span-end-repexp nil t))
+      (goto-char (match-end 0))
+      (insert
+       (concat
+        "<input "
+        "type=\"text\""
+        "name=\"simple-headline\""
+        "size=\"40\""
+        "maxlenght=\"80\">")))
+    (while (re-search-forward "<" nil t)
+      (goto-char (match-beginning 0))
+      (insert "<\input>"))
+    (buffer-substring-no-properties (point-min) (point-max))))
+
+(defun iorg-html-wrap-headline-in-form (transc-str back-end comm-chan) 
+  "Wrap the headline in the (BACK-END = html) transcoded Org headline TRANSC-STR into a html form, using information from the communication channel COMM-CHAN."
+    (with-temp-buffer
+    (insert transc-str)
+    (goto-char (point-min))
+    (while (re-search-forward iorg-html-outline-container-regexp nil t)
+      (goto-char (match-end 0))
+      (insert "<form action=\"http://localhost:8031/edit/\">"))
+    (while (re-search-forward iorg-html-div-end-repexp nil t)
+      (goto-char (match-beginning 0))
+      (insert
+        "<input type=\"submit\" value=\" Edit \" name=\"outline-container-1\">"
+        "</form>"))
+    (buffer-substring-no-properties (point-min) (point-max))))
+
+(defun iorg-html-wrap-headline-content-in-textarea (transc-str back-end comm-chan) 
+  "Wrap the content of the (BACK-END = html) transcoded Org headline TRANSC-STR into a html textarea, using information from the communication channel COMM-CHAN."
+    (with-temp-buffer
+    (insert transc-str)
+    (goto-char (point-min))
+    (while (re-search-forward iorg-html-outline-text-regexp nil t)
+      (goto-char (match-end 0))
+      (insert
+       (concat
+        "<textarea name=\"simple-content\""
+        "cols=\"79\" rows=\"30\">" )))
+    (while (re-search-forward iorg-html-div-end-repexp nil t)
+      (goto-char (match-beginning 0))
+      (insert "<\textarea>"))
+    (buffer-substring-no-properties (point-min) (point-max))))
+
 
 (defun iorg-launch (port)
   "Launch the elnode server which will serve and edit simple.org."
@@ -96,6 +258,24 @@
       ;(kill-buffer (current-buffer))
       )
     (iorg-initialize-simple-handler httpcon)))
+
+
+;; (defun iorg-edit-headline-handler (httpcon)
+;;   "Called by the elnode form handler to update headline."
+;;   ;; TODO: (3) handle form post data and update an Org-mode file
+;;   (message "entering `iorg-edit-headline-handler'")  
+;;   (let ((params (elnode-http-params httpcon)))
+;;     (message "These are the http-params: \n %s" params)
+;;     (with-current-buffer
+;;         (find-file (expand-file-name "simple.org" simple-dir))
+;;       (save-excursion
+;;         ;; (iorg--params-find-entry params)
+;;         ;; (org-todo 'done))
+;;       (save-buffer)
+;;       ;(kill-buffer (current-buffer))
+;;       )
+;;     (iorg-initialize-simple-handler httpcon))))
+  
 
 (defun iorg--get-outline-level (param-list)
   "Return level of outline-tree encoded in http-params"
@@ -161,28 +341,6 @@ in the Org file on that level."
              (file-name-nondirectory org-file))
             ".html") simple-dir )))))))
 
-(defun iorg-html-postprocess-headline (transc-str back-end comm-chan) 
-  "Post-process the static html in TRANSC-STR produced by the e-html BACK-END of the new Org exporter while transcoding an Org headline. Use the information in the communication channel COMM-CHAN for the post-processing.")
-
-(defun iorg-html-postprocess-paragraph (transc-str back-end comm-chan) 
-  "Post-process the static html in TRANSC-STR produced by the e-html BACK-END of the new Org exporter while transcoding an Org paragraph. Use the information in the communication channel COMM-CHAN for the post-processing.")
-
-(defun iorg-html--wrap-headline-content-in-textarea (transc-str &optional rows cols) 
-  "Wrap the content part of the html-transcoded Org headline TRANSC-STR into a html text-area, optionally setting the number of rows to ROWS and the number of columns to COLS."
-    (with-temp-buffer
-    (insert transc-str)
-    (goto-char (point-min))
-    (while (re-search-forward iorg-html-begin-txt-regexp nil t)
-      (goto-char (match-end 0))
-      (insert
-       (concat
-        "<textarea name=\"simple-content\""
-        "cols=\"79\" rows=\"30\">" )))
-    (while (re-search-forward iorg-html-div-end-repexp nil t)
-      (goto-char (match-beginning 0))
-      (insert "<\textarea>")
-    (buffer-substring-no-properties (point-min) (point-max))))
-)
 
 (defun iorg-404-handler (httpcon)
   ;; TODO: This should probably actually serve a 404 page rather than

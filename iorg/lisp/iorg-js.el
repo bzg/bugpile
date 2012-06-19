60a994994684c3680f7df6e375868d503ed2d071
Eric Schulte <eric.schulte@gmx.com>
2012-06-19 15:09:40 -0600

    renaming functions in light of the directory re-org

--- iorg/lisp/iorg-js.el
+++ iorg/lisp/iorg-js.el
@@ -1,4 +1,4 @@
-;;; org-e-iorg-js.el -- interactive HTML export
+;;; iorg-js.el -- interactive HTML export with JavaScript
 
 ;;; Code:
 (require 'org-export)
@@ -6,75 +6,75 @@
 (require 'org-e-html)
 (require 'iorg-util)
 
-(defvar org-e-iorg-js-style
+(defvar iorg-js-style
   (concat
    "<style type=\"text/css\">\n<!--/*--><![CDATA[/*><!--*/\n"
-   (file-contents (expand-file-name "org-e-iorg-js.css" iorg-base))
+   (file-contents (expand-file-name "iorg-js.css" iorg-base))
    "/*]]>*/-->\n</style>"))
 
-(defvar org-e-iorg-js-jquery
+(defvar iorg-js-jquery
   "https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js")
 
-(defvar org-e-iorg-js-scripts
+(defvar iorg-js-scripts
   (concat
-   "<script type=\"text/javascript\" src=\"" org-e-iorg-js-jquery "\"></script>"
+   "<script type=\"text/javascript\" src=\"" iorg-js-jquery "\"></script>"
    "<script type=\"text/javascript\">\n<!--/*--><![CDATA[/*><!--*/\n"
-   (file-contents (expand-file-name "org-e-iorg-js.js" iorg-base))
+   (file-contents (expand-file-name "iorg-js.js" iorg-base))
    "/*]]>*///-->\n</script>\n"))
 
-(defvar org-e-iorg-js-wrap-template
+(defvar iorg-js-wrap-template
   (concat "<div class=\"edit_in_place\">%html-text</div>"
           "<div class=\"raw-org\">%org-text</div>"))
 
-(defun org-e-iorg-js-interactive-p (element info)
+(defun iorg-js-interactive-p (element info)
   (let ((parent (org-export-get-parent element)))
     (cond ((eq (car parent) 'headline)
            (member "iorg" (org-export-get-tags parent info)))
           ((eq (car parent) 'org-data) nil)
           ((member (car parent) '(paragraph plain-list)) nil)
-          (t (org-e-iorg-js-interactive-p parent info)))))
+          (t (iorg-js-interactive-p parent info)))))
 
-(defmacro def-e-ihtml-wrap (e-html-function)
-  "Defines and returns an e-ihtml-wrapped version of E-HTML-FUNCTION."
-  (let ((fname (intern (concat "org-e-iorg-js"
+(defmacro def-iorg-js-wrap (e-html-function)
+  "Defines and returns an iorg-js-wrapped version of E-HTML-FUNCTION."
+  (let ((fname (intern (concat "iorg-js"
                                (substring (symbol-name e-html-function) 10)))))
     `(defun ,fname (element contents info)
-       ,(format "e-ihtml wrapper around `%s'." e-html-function)
+       ,(format "iorg-js wrapper around `%s'." e-html-function)
        (let* ((original-contents (copy-seq contents))
               (original-info     (copy-seq info))
               (html-text (,e-html-function element contents info))
               (org-text  (or (org-element-interpret-data element)
                              original-contents
                              "NIL")))
-         (if (org-e-iorg-js-interactive-p element info)
-             (org-fill-template org-e-iorg-js-wrap-template
+         (if (iorg-js-interactive-p element info)
+             (org-fill-template iorg-js-wrap-template
                                 (list (cons "html-text" html-text)
                                       (cons "org-text"  org-text)))
            html-text)))))
 
-(eval `(org-export-define-derived-backend e-ihtml e-html
+(eval `(org-export-define-derived-backend iorg-js e-html
          :translate-alist
-         ((paragraph  . ,(def-e-ihtml-wrap org-e-html-paragraph))
-          (plain-list . ,(def-e-ihtml-wrap org-e-html-plain-list)))))
+         ((paragraph  . ,(def-iorg-js-wrap html-paragraph))
+          (plain-list . ,(def-iorg-js-wrap html-plain-list)))))
 
-(defun org-e-iorg-js-export-to-html
+(defun iorg-js-export-to-html
   (&optional subtreep visible-only body-only ext-plist pub-dir)
   "Export current buffer to an interactive HTML file."
   (interactive)
-  (let* ((extension (concat "." org-e-html-extension))
+  (let* ((extension (concat "." html-extension))
 	 (file (org-export-output-file-name extension subtreep pub-dir))
-	 (org-export-coding-system org-e-html-coding-system)
+	 (org-export-coding-system html-coding-system)
          ;; custom headers
-         (org-e-html-style-extra org-e-iorg-js-style)
-         (org-e-html-scripts org-e-iorg-js-scripts))
-    (org-export-to-file 'e-ihtml
-                        file subtreep visible-only body-only ext-plist)))
+         (html-style-extra iorg-js-style)
+         (html-scripts iorg-js-scripts))
+    (org-export-to-file
+     'iorg-js file subtreep visible-only body-only ext-plist)))
 
-(defun org-e-iorg-js-export-file (file)
+(defun iorg-js-export-file (file)
   "Export FILE's contents to interactive HTML."
   (save-window-excursion
     (find-file file)
-    (org-e-iorg-js-export-to-html)))
+    (iorg-js-export-to-html)))
 
-(provide 'org-e-iorg-js)
-;;; org-e-iorg-js.el ends here
+(provide 'iorg-js)
+;;; iorg-js.el ends here

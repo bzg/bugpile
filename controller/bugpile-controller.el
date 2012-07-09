de85565f04b4d61a446638e89a046480cd2897f2
tj <tj@data-driven.de>
2012-07-09 11:53:40 +0200

    Some renaming. Implemenation of the search tasklist use-case (for prototype).

--- controller/bugpile-controller.el
+++ controller/bugpile-controller.el
@@ -32,16 +32,18 @@
 
 (defun bugpile-controller-index-handler (httpcon)
   "Serves the start-page of the Bugpile application"
+  (elnode-log-access "bugpile-controller" httpcon)
   (elnode-send-file httpcon
                     (iorg-controller--org-to-html
                      (concat
                       (iorg-projects-get-project-info
                        "bugpile" :view)
-                       "bugpile-index.org"))))
+                       "bugpile-view-index.org"))))
 
 
 (defun bugpile-controller-open-new-task-handler (httpcon)
   "Serve a form page for a new task and process user input."
+  (elnode-log-access "bugpile-controller" httpcon)  
   ;; (elnode-send-file httpcon
   ;;                   (iorg-controller--org-to-html
   ;;                    (concat
@@ -53,22 +55,24 @@
 (defun bugpile-controller-search-tasklist-handler (httpcon)
   "Serve a form page for query composition, process the user
 query and present the result list."
-  ;; (elnode-send-file httpcon
-  ;;                   (iorg-controller--org-to-html
-  ;;                    (concat
-  ;;                     (iorg-projects-get-project-info
-  ;;                      "bugpile" :view)
-  ;;                      "bugpile-index.org"))))
-)
+  (elnode-log-access "bugpile-controller" httpcon)
+  (elnode-send-file httpcon
+                    (iorg-controller--org-to-html
+                     (concat
+                      (iorg-projects-get-project-info
+                       "bugpile" :view)
+                       "bugpile-view-search-task-list.org"))))
+
 
 (defun bugpile-controller-take-action-on-selected-tasks-handler (httpcon)
   "Perform the action chosen by the user on all selected tasks."
-  ;; (elnode-send-file httpcon
-  ;;                   (iorg-controller--org-to-html
-  ;;                    (concat
-  ;;                     (iorg-projects-get-project-info
-  ;;                      "bugpile" :view)
-  ;;                      "bugpile-index.org"))))
-)
+  (elnode-log-access "bugpile-controller" httpcon)
+  (elnode-send-file httpcon
+                    (iorg-controller--org-to-html
+                     (concat
+                      (iorg-projects-get-project-info
+                       "bugpile" :view)
+                       "bugpile-view-take-action-on-selected-tasks.org"))))
+
 
 (provide 'bugpile-controller)
